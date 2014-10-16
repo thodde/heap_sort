66a635bed0e8272ff09b6c2ea03e497b839fde74
Trevor Hodde <thodde390@gmail.com>
2014-10-16 13:25:44 -0700

    adding code

---
+++ heap_sort.c
@@ -0,0 +1,219 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+void bottom_up_heapsort(int*, int);
+void heapsort(int*, int);
+void sift_up(int*, int);
+void sift_down(int*, int);
+void build_max_heap(int*, int); 
+void bottom_up_build_max_heap(int*, int);
+void randomize_in_place(int*, int);
+int* generate_array(int);
+void swap(int*, int*);
+void print_array(int*, int);
+
+int heapsize;
+int comparison_counter;
+clock_t begin, end;
+double time_spent;
+
+int main() {
+	int k, N;
+	int* A;
+	int* B;
+	int i;
+
+	printf("Testing Sift_Down Heap Sort\n");
+	for(k = 2; k <= 5; k++) {
+		comparison_counter = 0;
+		N = (int)pow((double)10, k);
+
+		begin = clock();
+		A = generate_array(N);
+		end = clock();
+		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		printf("Time Spent Generating Array: %f\n", time_spent);
+
+		// print the first unsorted array
+		//printf("Unsorted Array:\n");
+		//print_array(A, N);
+
+		begin = clock();
+		// call heapsort on the first unsorted array
+		heapsort(A, N);
+		end = clock();
+		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+		// show that the array is now sorted
+		//printf("Sorted array: \n");
+		//print_array(A, N);
+		printf("Done with k = %d\n", k);
+		printf("Number of comparisons: %d\n", comparison_counter);
+		printf("Time Spent on Heap Sort: %f\n", time_spent);
+		printf("\n");
+	}
+
+	printf("----------------------------------\n");
+	printf("Testing Sift_Up Heap Sort\n");
+        for(k = 2; k <= 5; k++) {
+		comparison_counter = 0;
+                N = (int)pow((double)10, k);
+
+		begin = clock();
+                B = generate_array(N);
+		end = clock();
+		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		printf("Time Spent Generating Array: %f\n", time_spent);
+
+                // print the unsorted array
+                //printf("Unsorted Array:\n");
+                //print_array(B, N);
+
+		begin = clock();
+                // call heapsort on the unsorted array
+                bottom_up_heapsort(B, N);
+		end = clock();
+		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+                // show that the array is now sorted
+                //printf("Sorted array: \n");
+                //print_array(B, N);
+                printf("Done with k = %d\n", k);
+		printf("Number of comparisons: %d\n", comparison_counter);
+		printf("Time Spent on Heap Sort: %f\n", time_spent);
+		printf("\n");
+        }
+
+	printf("----------------------------------\n");
+
+	return 0;
+}
+
+void bottom_up_heapsort(int* arr, int len) {
+	int i;
+
+	// build a max heap from the bottom up using sift up
+	bottom_up_build_max_heap(arr, len);
+	for(i = len-1; i >= 0; i--) {
+		// swap the last leaf and the root
+		swap(&arr[i], &arr[0]);
+		// remove the already sorted values
+		len--;
+		// repair the heap
+		bottom_up_build_max_heap(arr, len);
+	}
+}
+
+void heapsort(int* arr, int len) {
+	int i;
+
+	// build a max heap from the array
+	build_max_heap(arr, len);
+	for(i = len-1; i >= 1; i--) {
+		swap(&arr[0], &arr[i]); // move arr[0] to its sorted place
+		// remove the already sorted values
+		heapsize--;
+		sift_down(arr, 0);	// repair the heap
+	}
+}
+
+void sift_down(int* arr, int i) {
+	int c = 2*i+1;
+	int largest;
+
+	if(c >= heapsize) return;
+
+	if((c+1 < heapsize) && (arr[c+1] > arr[c])) {
+		c++;
+	}
+
+	if(arr[c] > arr[i]) {
+		comparison_counter++;
+		swap(&arr[c], &arr[i]);
+		sift_down(arr, c);
+	}
+}
+
+void sift_up(int* arr, int i) {
+	if(i == 0) return; // at the root
+
+	// if the current node is larger than its parent, swap them
+	if(arr[i] > arr[(i-1)/2]) {
+		comparison_counter++;
+		swap(&arr[i], &arr[(i-1)/2]);
+		// sift up to repair the heap
+		sift_up(arr, (i-1)/2);
+	}
+}
+
+void bottom_up_build_max_heap(int* arr, int len) {
+	int i;
+	for(i = 0; i < len; i++) {
+		sift_up(arr, i);
+	}
+}
+
+void build_max_heap(int* arr, int len) {
+	heapsize = len;
+	int i;
+	for(i = len/2; i >= 0; i--) {
+		// invariant: arr[k], i < k <= n are roots of proper heaps
+		sift_down(arr, i);
+	}
+}
+
+void randomize_in_place(int* arr, int n) {
+	int j, k;
+	double val;
+	time_t t;
+	// init the random number generator
+	srand((unsigned)time(&t));
+
+	// randomization code from class notes
+	for(j = 0; j < n-1; j++) {
+		val = ((double)random()) / 0x7FFFFFFF;
+		k = j + val*(n-j);
+		swap(&arr[k], &arr[j]);
+	}
+}
+
+// this function is responsible for creating and populating an array 
+// of size k, and randomizing the locations of its elements
+int* generate_array(int k) {
+	int* arr = (int*) malloc(sizeof(int)*k-1);
+	int i, j, x, N;
+	double val;
+	time_t t;
+	// init the random number generator
+	srand((unsigned)time(&t));
+
+	// fill the array with values from 1..N
+	for(i = 0; i <= k-1; i++) {
+		arr[i] = i+1;
+	}
+
+	N = (int)pow((double)10, 5);
+	// randomize the elements of the arry for 10^5 iterations
+	for(i = 0; i < N; i++) {
+		randomize_in_place(arr, k);
+	}
+
+	return arr;
+}
+
+// swap two elements
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+// print out an array by iterating through
+void print_array(int* arr, int size) {
+	int i;
+	for(i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
+}
